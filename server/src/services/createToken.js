298479b89d5b01fdf7c39b1e923fa0287de17d88
Dusan Machala <dusan.machala523gmail.com>
2023-03-06 09:45:47 +0100

    feat(src/services/createToken) create function for creating token for user

---
+++ server/src/services/createToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const createToken = (tokenDetails) => {
+  tokenDetails = { id: tokenDetails.id, email: tokenDetails.email };
+  return jwt.sign(tokenDetails, process.env.TOKEN_SECRET);
+};
+
+export default {
+  createToken,
+};
